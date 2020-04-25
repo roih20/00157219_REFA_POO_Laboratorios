f637e8be7835471dbe127fb67c7a1c717e677de3
roih20 <54415055+roih20@users.noreply.github.com>
2020-04-25 10:06:57 -0600

    Add files via upload

---
+++ Ejercicios de meet/Meet2304/meet/Persona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace meet
+{
+    public class Persona
+    {
+        private String nombre, apellido;
+        private int edad;
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+
+        public Persona(String nombre= "Sin nombre", 
+            String apellido = "Sin apellido"
+            , int edad = 0)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad; 
+        }
+
+        
+
+        public override string ToString()
+        {
+            return $"{nameof(nombre)}: {nombre}, " +
+                   $"{nameof(apellido)}: {apellido}," +
+                   $" {nameof(edad)}: {edad}";
+        }
+
+        public String saludar(String saludoP)
+        {
+            return nombre + "  " + saludoP;
+        }
+
+        public String getNombre()
+        {
+            return nombre;
+        }
+    }
+}
---
+++ Ejercicios de meet/Meet2304/meet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace meet
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+          var p = new List<Persona>();
+          
+          p.Add(new Persona("Rodrigo", "Figueroa",
+              19));
+          p.Add(new Persona("Walter", edad: 20));
+          p.Add(new Persona(edad: 25));
+          p.Add(new Persona(apellido: "Aguirre"));
+
+          String buscar = "Rodrigo";
+          buscar = Console.ReadLine();
+          p.ForEach(it =>
+          {
+
+              if (buscar.Equals(it.Nombre))
+              {
+                  Console.WriteLine("Nombre");
+                  it.Nombre = Console.ReadLine();
+                  Console.WriteLine(it.ToString());
+              }
+                  
+          });
+          
+          
+    }
+    }
+}
