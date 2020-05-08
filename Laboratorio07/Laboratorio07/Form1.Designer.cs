6fd6306bffce3a31fc7ecfeb346d5863eeb5b3c9
roih20 <54415055+roih20@users.noreply.github.com>
2020-05-08 01:03:01 -0600

    Add files via upload

---
+++ Laboratorio07/Laboratorio07/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Laboratorio07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.buttonbBlue = new System.Windows.Forms.Button();
+            this.buttonGreen = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Swis721 Cn BT", 20.25F, System.Drawing.FontStyle.Bold,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.Location = new System.Drawing.Point(36, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(563, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "presiona un boton";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.BackColor = System.Drawing.SystemColors.Menu;
+            this.buttonRed.Location = new System.Drawing.Point(21, 360);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(129, 48);
+            this.buttonRed.TabIndex = 1;
+            this.buttonRed.Text = "Rojo\r\n";
+            this.buttonRed.UseVisualStyleBackColor = false;
+            this.buttonRed.Click += new System.EventHandler(this.buttoRed_Click);
+            // 
+            // buttonbBlue
+            // 
+            this.buttonbBlue.BackColor = System.Drawing.SystemColors.Menu;
+            this.buttonbBlue.Location = new System.Drawing.Point(202, 360);
+            this.buttonbBlue.Name = "buttonbBlue";
+            this.buttonbBlue.Size = new System.Drawing.Size(141, 51);
+            this.buttonbBlue.TabIndex = 2;
+            this.buttonbBlue.Text = "azul\r\n";
+            this.buttonbBlue.UseVisualStyleBackColor = false;
+            this.buttonbBlue.Click += new System.EventHandler(this.buttonbBlue_Click);
+            // 
+            // buttonGreen
+            // 
+            this.buttonGreen.BackColor = System.Drawing.SystemColors.Menu;
+            this.buttonGreen.Location = new System.Drawing.Point(386, 360);
+            this.buttonGreen.Name = "buttonGreen";
+            this.buttonGreen.Size = new System.Drawing.Size(132, 51);
+            this.buttonGreen.TabIndex = 3;
+            this.buttonGreen.Text = "Verde\r\n";
+            this.buttonGreen.UseVisualStyleBackColor = false;
+            this.buttonGreen.Click += new System.EventHandler(this.buttonGreen_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Menu;
+            this.button1.Location = new System.Drawing.Point(316, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "cambiar color";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(177, 226);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 23);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Menu;
+            this.button2.Location = new System.Drawing.Point(217, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 44);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "cambiar color";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 519);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonGreen);
+            this.Controls.Add(this.buttonbBlue);
+            this.Controls.Add(this.buttonRed);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+      
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGreen;
+        private System.Windows.Forms.Button buttonbBlue;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Laboratorio07/Laboratorio07/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.DataSource = new List<String>() {"Morado", "Celeste", "Rosado"};
+        }
+
+
+
+        private void buttonbBlue_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Blue;
+        }
+
+        private void buttonGreen_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Green;
+        }
+
+        private void buttoRed_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BackColor = System.Drawing.ColorTranslator.FromHtml("#" + textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String Morado = "#4B088A", celeste = "#81F7F3", rosado = " #FE2EF7";
+
+            switch (comboBox1.SelectedItem)
+            {
+                case 0:
+                    BackColor = ColorTranslator.FromHtml(Morado);
+                    break;
+                
+                case 1:
+                    BackColor = ColorTranslator.FromHtml(celeste);
+                    break;
+                
+                case 2:
+                    BackColor = ColorTranslator.FromHtml(rosado);
+                    break;
+                
+            }
+
+       
+           
+        }
+
+    }
+
+}
